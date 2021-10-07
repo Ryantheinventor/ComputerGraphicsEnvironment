d77725ee3ca0e860b520b992d8e27485473ace27
Ryantheinventor <ryantheinventor1@gmail.com>
2021-10-06 17:46:23 -0700

    Added turret targeting script

---
+++ Assets/Scripts/Enemies/TurretHeadTageting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretHeadTageting : MonoBehaviour
+{
+    public Rigidbody target;
+    private Rigidbody myRB;
+
+
+    private void Start() 
+    {
+        myRB = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() 
+    {
+        myRB.rotation = Quaternion.LookRotation(target.position - myRB.position, Vector3.up);
+    }
+
+
+}
