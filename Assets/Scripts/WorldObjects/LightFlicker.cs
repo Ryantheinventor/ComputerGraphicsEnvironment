7fbee26802e82004d3cb3f9cbfa1739ff63ce4cd
Ryantheinventor <ryantheinventor1@gmail.com>
2021-10-27 17:33:05 -0700

    Added lighting to the environment

---
+++ Assets/Scripts/WorldObjects/LightFlicker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    public float timeBetweenAttempt = 0.4f;
+    private float curTime = 0;
+    [Range(0,1)]
+    public float chance = 0.5f;
+    public GameObject light;
+    private void Update() 
+    {
+        if(light)
+        {
+            curTime += Time.deltaTime;
+            if(curTime>=timeBetweenAttempt)
+            {
+                int iChance = (int)(chance * 100);
+                if(Random.RandomRange(0,100) <= iChance)
+                {
+                    light.SetActive(!light.activeSelf);
+                }
+                curTime = 0;
+            }
+        }
+    }
+}
