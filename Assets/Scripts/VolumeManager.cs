9d1307d45017f6a751b4a4b4e4db82494f9d363d
Ryantheinventor <ryantheinventor1@gmail.com>
2021-11-18 11:36:39 -0800

    Added Audio Mixer, volume control and ambiance wind

---
+++ Assets/Scripts/VolumeManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using TMPro;
+
+public class VolumeManager : MonoBehaviour
+{
+    public float maxVolume = 0;
+    public float minVolume = -80;
+    public AudioMixer mixer;
+    public TextMeshProUGUI textMesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float value = PlayerPrefs.GetFloat("MVolume",100);
+        textMesh.text = value + "%";
+
+        value = Remap(value, 0, 100, minVolume, maxVolume);
+        mixer.SetFloat("MasterVolume", value);
+    }
+
+    public void VolumeChange(float amount = 0.1f)
+    {
+        float newValue = PlayerPrefs.GetFloat("MVolume",100) + amount;
+        newValue = newValue > 100 ? 100 : newValue;
+        newValue = newValue < 0 ? 0 : newValue;
+
+        textMesh.text = newValue + "%";
+        PlayerPrefs.SetFloat("MVolume", newValue);
+
+        newValue = Remap(newValue, 0, 100, minVolume, maxVolume);
+        mixer.SetFloat("MasterVolume", newValue);
+    }
+    
+    private float Remap(float value, float baseMin, float baseMax, float targetMin, float targetMax)
+    {
+        value -= baseMin;
+        baseMax -= baseMin;
+        value /= baseMax;
+
+        targetMax -= targetMin;
+        value *= targetMax;
+        value += targetMin;
+
+        return value;
+    }
+
+
+}
--- README.txt
+++ README.txt
@@ -3,10 +3,12 @@ Test project for unity environment tools
 Unity 2020.1.10f1
 
 External Assets:
-Basic Motions Free (https://assetstore.unity.com/packages/3d/animations/basic-motions-free-154271#description)
-Kenny Assets:
-    Game Icons (https://kenney.nl/assets/game-icons)
-    Particles Pack (https://kenney.nl/assets/particle-pack)
-freesound.org:
-    https://freesound.org/people/Tomlija/sounds/103333/
-    https://freesound.org/people/ejfortin/sounds/49693/
+    Basic Motions Free (https://assetstore.unity.com/packages/3d/animations/basic-motions-free-154271#description)
+    Kenny Assets:
+        Game Icons (https://kenney.nl/assets/game-icons)
+        Particles Pack (https://kenney.nl/assets/particle-pack)
+    Sound assets from freesound.org:
+        https://freesound.org/people/Tomlija/sounds/103333/
+        https://freesound.org/people/ejfortin/sounds/49693/
+        https://freesound.org/people/Garuda1982/sounds/563047/
+        https://freesound.org/people/uzerx/sounds/63138/
