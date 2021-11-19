f564a519a1a6a3d61577c731b022d7ecc16fb4ee
Ryantheinventor <ryantheinventor1@gmail.com>
2021-11-19 09:27:00 -0800

    Changed some scene loading to Async

--- Assets/Scripts/LoadSceneOnCall.cs
+++ Assets/Scripts/LoadSceneOnCall.cs
@@ -6,6 +6,25 @@ public class LoadSceneOnCall : MonoBehaviour
 {
     public void Load(int id)
     {
-        SceneManager.LoadScene(id);
+        StartCoroutine(LoadAsyncScene(id));
     }
+
+    IEnumerator LoadAsyncScene(int id)
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(id);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            Debug.Log(asyncLoad.progress);
+            yield return null;
+        }
+    }
+
+
 }
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,7 +9,24 @@ public class MainMenu : MonoBehaviour
     public int sceneToLoadID = 1;
     public void OnPlay()
     {
-        SceneManager.LoadScene(sceneToLoadID);
+        StartCoroutine(LoadAsyncScene(sceneToLoadID));
+    }
+
+    IEnumerator LoadAsyncScene(int id)
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(id);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            Debug.Log(asyncLoad.progress);
+            yield return null;
+        }
     }
 
     public void OnQuit()
